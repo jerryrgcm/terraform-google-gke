647ecf55729f91cdc4292c9526ff186f3c78841b
jerryrgcm <jerryr_gcm@yahoo.com>
2020-05-04 17:11:05 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -4,6 +4,7 @@
 # ---------------------------------------------------------------------------------------------------------------------
 
 
+
 variable "project" {
   description = "The project ID where all resources will be launched."
   type        = string
